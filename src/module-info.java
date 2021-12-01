c3a333186361bd380ebbc2b6ba319072805f50f1
Ismoil <ismoil6ek@gmail.com>
2021-12-02 00:59:12 +0500

    fuck

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module group.project.java.coders {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.base;
+    requires javafx.graphics;
 
     opens loginDialog;
     opens studentUser;
