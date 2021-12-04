60368d87bbe1492b27665a99e54567871bb19824
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2021-12-04 20:43:44 +0500

    sdfadfsd

--- University Management System/src/university/management/system/FeeStructure.java
+++ University Management System/src/university/management/system/FeeStructure.java
@@ -10,12 +10,11 @@ import java.awt.*;
 
 public class FeeStructure extends JFrame{
     
-    JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17;
+    JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11;
     JLabel i1;
     JLabel b1,b2,b3,b4,b5,b6,b7,b8;
-    JLabel bb1,bb2,bb3,bb4,bb5,bb6,bb7,bb8,bb9,bb10,bb11,bb12,bb13,bb14,bb15,bb16,bb17,bb18;
-    JLabel m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12;
-    
+    JLabel bb1,bb2,bb3,bb4,bb5,bb6,bb7,bb8;
+
     public FeeStructure(){
         
         setSize(1700,800);
@@ -24,7 +23,7 @@ public class FeeStructure extends JFrame{
         
         i1 = new JLabel("Fee Structure");
         i1.setFont(new Font("serif",Font.ITALIC,56));
-        i1.setBounds(600,10,400,70);
+        i1.setBounds(400,10,400,70);
         add(i1);
         
         l1 = new JLabel("Course");
@@ -32,272 +31,137 @@ public class FeeStructure extends JFrame{
         l1.setFont(new Font("serif",Font.BOLD,26));
         add(l1);
         
-        l2 = new JLabel("BTech");
-        l2.setBounds(280,100,100,40);
+        l2 = new JLabel("SOCIE");
+        l2.setBounds(480,100,100,40);
         l2.setFont(new Font("serif",Font.BOLD,26));
         add(l2);
         
-        l3 = new JLabel("BCA");
-        l3.setBounds(480,100,100,40);
+        l3 = new JLabel("SOL");
+        l3.setBounds(880,100,100,40);
         l3.setFont(new Font("serif",Font.BOLD,26));
         add(l3);
-        
-        l4 = new JLabel("BBA");
-        l4.setBounds(680,100,100,40);
+
+        l4 = new JLabel("Semester 1");
+        l4.setBounds(80,170,150,40);
         l4.setFont(new Font("serif",Font.BOLD,26));
         add(l4);
         
-        bb5 = new JLabel("Rs 25000");
-        bb5.setBounds(680,170,150,40);
-        bb5.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb5);
-        
-        bb6 = new JLabel("Rs 25000");
-        bb6.setBounds(680,240,150,40);
-        bb6.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb6);
-        
-        bb7 = new JLabel("Rs 25000");
-        bb7.setBounds(680,310,150,40);
-        bb7.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb7);
-        
-        bb8 = new JLabel("Rs 25000");
-        bb8.setBounds(680,380,150,40);
-        bb8.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb8);
-        
-        bb13 = new JLabel("Rs 25000");
-        bb13.setBounds(680,450,150,40);
-        bb13.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb13);
-        
-        bb14 = new JLabel("Rs 25000");
-        bb14.setBounds(680,520,150,40);
-        bb14.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb14);
-        
-        l5 = new JLabel("Bsc");
-        l5.setBounds(880,100,100,40);
+        l5 = new JLabel("Semester 2");
+        l5.setBounds(80,240,150,40);
         l5.setFont(new Font("serif",Font.BOLD,26));
         add(l5);
         
-        bb9 = new JLabel("Rs 18000");
-        bb9.setBounds(880,170,150,40);
-        bb9.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb9);
-        
-        bb10 = new JLabel("Rs 18000");
-        bb10.setBounds(880,240,150,40);
-        bb10.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb10);
-        
-        bb11 = new JLabel("Rs 18000");
-        bb11.setBounds(880,310,150,40);
-        bb11.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb11);
-        
-        bb12 = new JLabel("Rs 18000");
-        bb12.setBounds(880,380,150,40);
-        bb12.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb12);
-        
-        bb15 = new JLabel("Rs 18000");
-        bb15.setBounds(880,450,150,40);
-        bb15.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb15);
-        
-        bb16 = new JLabel("Rs 18000");
-        bb16.setBounds(880,520,150,40);
-        bb16.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb16);
-        
-        l6 = new JLabel("MBA");
-        l6.setBounds(1080,100,100,40);
+        l6 = new JLabel("Semester 3");
+        l6.setBounds(80,310,150,40);
         l6.setFont(new Font("serif",Font.BOLD,26));
         add(l6);
         
-        m1 = new JLabel("Rs 65000");
-        m1.setBounds(1080,170,150,40);
-        m1.setFont(new Font("serif",Font.PLAIN,26));
-        add(m1);
-        
-        m2 = new JLabel("Rs 65000");
-        m2.setBounds(1080,240,150,40);
-        m2.setFont(new Font("serif",Font.PLAIN,26));
-        add(m2);
-        
-        m3 = new JLabel("Rs 65000");
-        m3.setBounds(1080,310,150,40);
-        m3.setFont(new Font("serif",Font.PLAIN,26));
-        add(m3);
-        
-        m4 = new JLabel("Rs 65000");
-        m4.setBounds(1080,380,150,40);
-        m4.setFont(new Font("serif",Font.PLAIN,26));
-        add(m4);
-        
-        l7 = new JLabel("MCA");
-        l7.setBounds(1280,100,100,40);
+        l7 = new JLabel("Semester 4");
+        l7.setBounds(80,380,150,40);
         l7.setFont(new Font("serif",Font.BOLD,26));
         add(l7);
         
-        m5 = new JLabel("Rs 55000");
-        m5.setBounds(1280,170,150,40);
-        m5.setFont(new Font("serif",Font.PLAIN,26));
-        add(m5);
-        
-        m6 = new JLabel("Rs 55000");
-        m6.setBounds(1280,240,150,40);
-        m6.setFont(new Font("serif",Font.PLAIN,26));
-        add(m6);
-        
-        m7 = new JLabel("Rs 55000");
-        m7.setBounds(1280,310,150,40);
-        m7.setFont(new Font("serif",Font.PLAIN,26));
-        add(m7);
-        
-        m8 = new JLabel("Rs 55000");
-        m8.setBounds(1280,380,150,40);
-        m8.setFont(new Font("serif",Font.PLAIN,26));
-        add(m8);
-        
-        l8 = new JLabel("MTech");
-        l8.setBounds(1480,100,100,40);
+        l8 = new JLabel("Semester 5");
+        l8.setBounds(80,450,150,40);
         l8.setFont(new Font("serif",Font.BOLD,26));
         add(l8);
         
-        m9 = new JLabel("Rs 81000");
-        m9.setBounds(1480,170,150,40);
-        m9.setFont(new Font("serif",Font.PLAIN,26));
-        add(m9);
+        l9 = new JLabel("Semester 6");
+        l9.setBounds(80,520,150,40);
+        l9.setFont(new Font("serif",Font.BOLD,26));
+        add(l9);
         
-        m10 = new JLabel("Rs 81000");
-        m10.setBounds(1480,240,150,40);
-        m10.setFont(new Font("serif",Font.PLAIN,26));
-        add(m10);
-        
-        m11 = new JLabel("Rs 81000");
-        m11.setBounds(1480,310,150,40);
-        m11.setFont(new Font("serif",Font.PLAIN,26));
-        add(m11);
-        
-        m12 = new JLabel("Rs 81000");
-        m12.setBounds(1480,380,150,40);
-        m12.setFont(new Font("serif",Font.PLAIN,26));
-        add(m12);
-        
-        l10 = new JLabel("Semester 1");
-        l10.setBounds(80,170,150,40);
+        l10 = new JLabel("Semester 7");
+        l10.setBounds(80,590,150,40);
         l10.setFont(new Font("serif",Font.BOLD,26));
         add(l10);
         
-        l11 = new JLabel("Semester 2");
-        l11.setBounds(80,240,150,40);
+        l11 = new JLabel("Semester 8");
+        l11.setBounds(80,660,150,40);
         l11.setFont(new Font("serif",Font.BOLD,26));
         add(l11);
         
-        l12 = new JLabel("Semester 3");
-        l12.setBounds(80,310,150,40);
-        l12.setFont(new Font("serif",Font.BOLD,26));
-        add(l12);
         
-        l13 = new JLabel("Semester 4");
-        l13.setBounds(80,380,150,40);
-        l13.setFont(new Font("serif",Font.BOLD,26));
-        add(l13);
-        
-        l14 = new JLabel("Semester 5");
-        l14.setBounds(80,450,150,40);
-        l14.setFont(new Font("serif",Font.BOLD,26));
-        add(l14);
-        
-        l15 = new JLabel("Semester 6");
-        l15.setBounds(80,520,150,40);
-        l15.setFont(new Font("serif",Font.BOLD,26));
-        add(l15);
-        
-        l16 = new JLabel("Semester 7");
-        l16.setBounds(80,590,150,40);
-        l16.setFont(new Font("serif",Font.BOLD,26));
-        add(l16);
-        
-        l17 = new JLabel("Semester 8");
-        l17.setBounds(80,660,150,40);
-        l17.setFont(new Font("serif",Font.BOLD,26));
-        add(l17);
-        
-        
-        b1 = new JLabel("Rs 43000");
-        b1.setBounds(280,170,100,40);
+        b1 = new JLabel("15,000,000 UZS");
+        b1.setBounds(480,170,300,40);
         b1.setFont(new Font("serif",Font.PLAIN,26));
         add(b1);
         
-        b2 = new JLabel("Rs 43000");
-        b2.setBounds(280,240,100,40);
+        b2 = new JLabel("15,000,000 UZS");
+        b2.setBounds(480,240,300,40);
         b2.setFont(new Font("serif",Font.PLAIN,26));
         add(b2);
         
-        b3 = new JLabel("Rs 43000");
-        b3.setBounds(280,310,100,40);
+        b3 = new JLabel("15,000,000 UZS");
+        b3.setBounds(480,310,300,40);
         b3.setFont(new Font("serif",Font.PLAIN,26));
         add(b3);
         
-        b4 = new JLabel("Rs 43000");
-        b4.setBounds(280,380,100,40);
+        b4 = new JLabel("15,000,000 UZS");
+        b4.setBounds(480,380,300,40);
         b4.setFont(new Font("serif",Font.PLAIN,26));
         add(b4);
         
-        b5 = new JLabel("Rs 43000");
-        b5.setBounds(280,450,100,40);
+        b5 = new JLabel("15,000,000 UZS");
+        b5.setBounds(480,450,300,40);
         b5.setFont(new Font("serif",Font.PLAIN,26));
         add(b5);
         
-        b6 = new JLabel("Rs 43000");
-        b6.setBounds(280,520,100,40);
+        b6 = new JLabel("15,000,000 UZS");
+        b6.setBounds(480,520,300,40);
         b6.setFont(new Font("serif",Font.PLAIN,26));
         add(b6);
         
-        b7 = new JLabel("Rs 43000");
-        b7.setBounds(280,590,100,40);
+        b7 = new JLabel("15,000,000 UZS");
+        b7.setBounds(480,590,300,40);
         b7.setFont(new Font("serif",Font.PLAIN,26));
         add(b7);
         
-        b8 = new JLabel("Rs 43000");
-        b8.setBounds(280,660,100,40);
+        b8 = new JLabel("15,000,000 UZS");
+        b8.setBounds(480,660,300,40);
         b8.setFont(new Font("serif",Font.PLAIN,26));
         add(b8);
         
         
-        bb1 = new JLabel("Rs 32000");
-        bb1.setBounds(480,170,100,40);
+        bb1 = new JLabel("15,750,000 UZS");
+        bb1.setBounds(880,170,300,40);
         bb1.setFont(new Font("serif",Font.PLAIN,26));
         add(bb1);
         
-        bb2 = new JLabel("Rs 32000");
-        bb2.setBounds(480,240,100,40);
+        bb2 = new JLabel("15,750,000 UZS");
+        bb2.setBounds(880,240,300,40);
         bb2.setFont(new Font("serif",Font.PLAIN,26));
         add(bb2);
         
-        bb3 = new JLabel("Rs 32000");
-        bb3.setBounds(480,310,100,40);
+        bb3 = new JLabel("15,750,000 UZS");
+        bb3.setBounds(880,310,300,40);
         bb3.setFont(new Font("serif",Font.PLAIN,26));
         add(bb3);
         
-        bb4 = new JLabel("Rs 32000");
-        bb4.setBounds(480,380,100,40);
+        bb4 = new JLabel("15,750,000 UZS");
+        bb4.setBounds(880,380,300,40);
         bb4.setFont(new Font("serif",Font.PLAIN,26));
         add(bb4);
         
-        bb17 = new JLabel("Rs 32000");
-        bb17.setBounds(480,450,100,40);
-        bb17.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb17);
+        bb5 = new JLabel("15,750,000 UZS");
+        bb5.setBounds(880,450,300,40);
+        bb5.setFont(new Font("serif",Font.PLAIN,26));
+        add(bb5);
         
-        bb18 = new JLabel("Rs 32000");
-        bb18.setBounds(480,520,100,40);
-        bb18.setFont(new Font("serif",Font.PLAIN,26));
-        add(bb18);
+        bb6 = new JLabel("15,750,000 UZS");
+        bb6.setBounds(880,520,300,40);
+        bb6.setFont(new Font("serif",Font.PLAIN,26));
+        add(bb6);
+
+        bb7 = new JLabel("15,750,000 UZS");
+        bb7.setBounds(880,590,300,40);
+        bb7.setFont(new Font("serif",Font.PLAIN,26));
+        add(bb7);
+
+        bb8 = new JLabel("15,750,000 UZS");
+        bb8.setBounds(880,660,300,40);
+        bb8.setFont(new Font("serif",Font.PLAIN,26));
+        add(bb8);
         
         getContentPane().setBackground(Color.WHITE);
         setVisible(true);
